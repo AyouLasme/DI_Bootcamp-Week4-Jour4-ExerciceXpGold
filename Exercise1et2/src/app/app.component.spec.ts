f9f05d55696a60f6094e68f1643ca7d195c1fea5
Ayou Lasme <ayou.lasme@mail.com>
2023-01-26 18:47:00 +0000

    Exercice Xp Gold Week4 Jour4

--- Exercise1et2/src/app/app.component.spec.ts
+++ Exercise1et2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ExerciceXpGold'`, () => {
+  it(`should have as title 'XPGOLD'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ExerciceXpGold');
+    expect(app.title).toEqual('XPGOLD');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ExerciceXpGold app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('XPGOLD app is running!');
   });
 });
--- Exercise1et2/src/app/app.component.ts
+++ Exercise1et2/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ExerciceXpGold';
+
+  location = {
+    address: "BP 01 KernelVille"
+  }
+
+  submitForm(value: any) {
+    console.log( value )
+  }
 }
